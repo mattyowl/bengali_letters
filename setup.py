e2f8175b5df3ea6f97114e95ff1e7387b32832c1
Matt Hilton <mattyowl@gmail.com>
2020-04-12 11:13:15 +0200

    Edited setup script

--- setup.py
+++ setup.py
@@ -12,15 +12,12 @@ cmdclass=versioneer.get_cmdclass()
 setup(name='bengali_letters',
       version=versioneer.get_version(),
       cmdclass=cmdclass,
-      #url="https://acru.ukzn.ac.za/~mjh/nemo",
       author='Matt Hilton',
       author_email='matt.hilton@mykolab.com',
       classifiers=[],
       description='Quiz app for learning Bengali letters.',
       long_description="""Multiple choice quiz app for learning Bengali letters.""",
       packages=['bengali_letters'],
-      #package_data={'nemo': ['data/*']},
       scripts=['bin/bengali_letters'],
-      #ext_modules=[Extension("nemoCython", ["nemo/nemoCython.pyx"], include_dirs=[numpy.get_include()])],
       install_requires=["numpy"]
 )
